eae1cf8db4aabeb15ab66ee56e9d3a0cd358193c
spiros <s.denaxas@gmail.com>
2019-01-04 12:29:45 +0200

    two more tests with more points

--- frechetdist/tests/test_all.py
+++ frechetdist/tests/test_all.py
@@ -1,12 +1,15 @@
 
 import pytest
 import sys, os
+import numpy as np
+
 myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../')
 
 from frechetdist import frdist
 
 test_cases = [
+    
     {
         'P': [ [1,1], [2,1], [2,2] ],
         'Q': [ [2,2], [0,1], [2,4] ],
@@ -14,8 +17,8 @@ test_cases = [
     },
 
     {
-        'P': [[1,1], [2,1], [2,2]],
-        'Q': [[1,1], [2,1], [2,2]],
+        'P': np.array((np.linspace(0.0, 1.0, 100), np.ones(100))).T,
+        'Q': np.array((np.linspace(0.0, 1.0, 100), np.ones(100))).T,
         'expected': 0
     },
     
@@ -23,7 +26,14 @@ test_cases = [
         'P': [ [-1,0], [0,1], [1,0], [0, -1] ],
         'Q': [ [-2,0], [0,2], [2,0], [0, -2] ],
         'expected': 1.0
+    },
+
+    {
+        'P': np.array((np.linspace(0.0, 1.0, 100), np.ones(100)*2)).T,
+        'Q': np.array((np.linspace(0.0, 1.0, 100), np.ones(100))).T,
+        'expected': 1.0
     }
+
 ]
 
 def test_main():
