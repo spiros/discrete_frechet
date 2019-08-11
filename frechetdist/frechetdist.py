d9ba49e9a7ca55d1efe8cfff89f41b7d8b7d9121
spiros <s.denaxas@gmail.com>
2019-08-11 16:51:08 +0800

    lint code and tests

--- frechetdist/frechetdist.py
+++ frechetdist/frechetdist.py
@@ -1,50 +1,50 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import math
 import numpy as np
 
-__all__ = [ 'frdist' ]
+__all__ = ['frdist']
 
-def _c(ca,i,j,p,q):
 
-    if ca[i,j] > -1:
-        return ca[i,j]
+def _c(ca, i, j, p, q):
+
+    if ca[i, j] > -1:
+        return ca[i, j]
     elif i == 0 and j == 0:
-        ca[i,j] = np.linalg.norm(p[i]-q[j])
+        ca[i, j] = np.linalg.norm(p[i]-q[j])
     elif i > 0 and j == 0:
-        ca[i,j] = max( _c(ca,i-1,0,p,q), np.linalg.norm(p[i]-q[j]) )
+        ca[i, j] = max(_c(ca, i-1, 0, p, q), np.linalg.norm(p[i]-q[j]))
     elif i == 0 and j > 0:
-        ca[i,j] = max( _c(ca,0,j-1,p,q), np.linalg.norm(p[i]-q[j]) )
+        ca[i, j] = max(_c(ca, 0, j-1, p, q), np.linalg.norm(p[i]-q[j]))
     elif i > 0 and j > 0:
-        ca[i,j] = max(                                                     \
-            min(                                                           \
-                _c(ca,i-1,j,p,q),                                          \
-                _c(ca,i-1,j-1,p,q),                                        \
-                _c(ca,i,j-1,p,q)                                           \
-            ),                                                             \
-            np.linalg.norm(p[i]-q[j])                                            \
-            )                                                          
+        ca[i, j] = max(
+            min(
+                _c(ca, i-1, j, p, q),
+                _c(ca, i-1, j-1, p, q),
+                _c(ca, i, j-1, p, q)
+            ),
+            np.linalg.norm(p[i]-q[j])
+            )
     else:
-        ca[i,j] = float('inf')
-    
-    return ca[i,j]
+        ca[i, j] = float('inf')
+
+    return ca[i, j]
 
 
-def frdist(p,q):
-    """ 
+def frdist(p, q):
+    """
     Computes the discrete Fréchet distance between
     two curves. The Fréchet distance between two curves in a
     metric space is a measure of the similarity between the curves.
     The discrete Fréchet distance may be used for approximately computing
-    the Fréchet distance between two arbitrary curves, 
+    the Fréchet distance between two arbitrary curves,
     as an alternative to using the exact Fréchet distance between a polygonal
     approximation of the curves or an approximation of this value.
-    
+
     This is a Python 3.* implementation of the algorithm produced
-    in Eiter, T. and Mannila, H., 1994. Computing discrete Fréchet distance. Tech. 
-    Report CD-TR 94/64, Information Systems Department, Technical University 
-    of Vienna.
+    in Eiter, T. and Mannila, H., 1994. Computing discrete Fréchet distance.
+    Tech. Report CD-TR 94/64, Information Systems Department, Technical
+    University of Vienna.
     http://www.kr.tuwien.ac.at/staff/eiter/et-archive/cdtr9464.pdf
 
     Function dF(P, Q): real;
@@ -56,7 +56,7 @@ def frdist(p,q):
                 if ca(i, j) > −1 then return ca(i, j)
                 elsif i = 1 and j = 1 then ca(i, j) := d(u1, v1)
                 elsif i > 1 and j = 1 then ca(i, j) := max{ c(i − 1, 1), d(ui, v1) }
-                elsif i = 1 and j > 1 then ca(i, j) := max{ c(1, j − 1), d(u1, vj ) }
+                elsif i = 1 and j > 1 then ca(i, j) := max{ c(1, j − 1), d(u1, vj) }
                 elsif i > 1 and j > 1 then ca(i, j) :=
                 max{ min(c(i − 1, j), c(i − 1, j − 1), c(i, j − 1)), d(ui, vj ) }
                 else ca(i, j) = ∞
@@ -102,7 +102,7 @@ def frdist(p,q):
     if len_p != len_q or len(p[0]) != len(q[0]):
         raise ValueError('Input curves do not have the same dimensions.')
 
-    ca    = ( np.ones((len_p,len_q), dtype=np.float64) * -1 ) 
+    ca = (np.ones((len_p, len_q), dtype=np.float64) * -1)
 
-    dist = _c(ca,len_p-1,len_q-1,p,q)
+    dist = _c(ca, len_p-1, len_q-1, p, q)
     return dist
--- frechetdist/tests/test_all.py
+++ frechetdist/tests/test_all.py
@@ -1,18 +1,19 @@
 
-import pytest
-import sys, os
+import sys
+import os
 import numpy as np
+import pytest
 
-myPath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, myPath + '/../')
+MYPATH = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, MYPATH + '/../')
 
 from frechetdist import frdist
 
-test_cases = [
-    
+TEST_CASES = [
+
     {
-        'P': [ [1,1], [2,1], [2,2] ],
-        'Q': [ [2,2], [0,1], [2,4] ],
+        'P': [[1, 1], [2, 1], [2, 2]],
+        'Q': [[2, 2], [0, 1], [2, 4]],
         'expected': 2.0
     },
 
@@ -21,10 +22,10 @@ test_cases = [
         'Q': np.array((np.linspace(0.0, 1.0, 100), np.ones(100))).T,
         'expected': 0
     },
-    
+
     {
-        'P': [ [-1,0], [0,1], [1,0], [0, -1] ],
-        'Q': [ [-2,0], [0,2], [2,0], [0, -2] ],
+        'P': [[-1, 0], [0, 1], [1, 0], [0, -1]],
+        'Q': [[-2, 0], [0, 2], [2, 0], [0, -2]],
         'expected': 1.0
     },
 
@@ -36,23 +37,26 @@ test_cases = [
 
 ]
 
+
 def test_main():
-    for test_case in test_cases:
+    for test_case in TEST_CASES:
         P = test_case['P']
         Q = test_case['Q']
         eo = test_case['expected']
 
-        assert frdist(P,Q) == eo
+        assert frdist(P, Q) == eo
+
 
 def test_errors():
 
-    P=[[1,1], [2,1]]
-    Q=[[2,2], [0,1], [2,4]]
-    with pytest.raises(ValueError):
-        assert frdist(P,Q) == 2.0
+    P = [[1, 1], [2, 1]]
+    Q = [[2, 2], [0, 1], [2, 4]]
 
-    P=[]
-    Q=[[2,2], [0,1], [2,4]]
     with pytest.raises(ValueError):
-        assert frdist(P,Q) == 2.0
-    
+        assert frdist(P, Q) == 2.0
+
+    P = []
+    Q = [[2, 2], [0, 1], [2, 4]]
+
+    with pytest.raises(ValueError):
+        assert frdist(P, Q) == 2.0
